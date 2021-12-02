a6c8a3c609d15f77e4cb4df9e042aa20a7a552a3
Sandra Jimenez <sjimenez@partner.fluidra.com>
2021-12-02 13:20:53 +0100

    test adding function on env dev and push to master (check if deve deploys the function)

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cognitocf0c6096": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "AppClientSecret": "string"
+        }
+    },
+    "function": {
+        "test": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    }
+}
